345e1fbc361fef952ca54ab65614522bd7c98df6
mominaabid <mominaabid789@gmail.com>
2026-06-24 07:59:31 +0500

    wsgi fixed

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -3,7 +3,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    os.getenv('DJANGO_SETTINGS_MODULE', 'core.settings')
+    'core.settings.development'  # or 'core.settings' if prod
 )
 
 application = get_wsgi_application()
